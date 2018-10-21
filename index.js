059e432ddd3ae979d0ad7692e349a396e193cea9
lodu23 <44348883+lodu23@users.noreply.github.com>
2018-10-22 01:06:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
-const token = process.env.token // a garder en version heroku
-const prefix = ("?");
-
-
-
+const cfg require('./index.json');
+const token = process.env.token;
+const prefix = (cfg.prefix);
+bot.on('ready',function () (
+  console.log("c'est partie !!!")
+  bot.user.setActivity(Regarder Nathalie').catch(console.error)
+  });
 
 
 
